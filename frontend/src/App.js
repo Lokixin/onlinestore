8350156e341255d8b878720920ccd044177d203e
Dimas Ávila <dimasavilamartinez@gmail.com>
2020-10-04 16:30:23 +0200

    HomePage Screen, Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Iravimart</h1>
+    <Header/>
+    <main className="py-3">
+      <Container className="text-center">
+        <h1>Welcome to Iravimart</h1>
+        <HomeScreen/>
+      </Container>
+
+    </main>
+    <Footer/>
     </>
   );
 }
