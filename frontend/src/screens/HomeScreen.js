1849769e4737f39a185c6fdbec69df76322b9367
Dimas Ávila <dimasavilamartinez@gmail.com>
2020-10-10 19:42:19 +0200

    Single product reducers, productlist reducers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Col, Container, Row } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 
@@ -17,8 +19,11 @@ const HomeScreen = () => {
 
     return (
         <>
-            {loading ? <h2>Loading ... </h2>: error ? <h3>{error}</h3>
-            :( <> <Container className="ml-3"><h1>Welcome to iravimar</h1></Container>
+            {loading ? (
+                <Loader/>
+            ) : error ? (
+                <Message/>
+            ) : ( <> <Container className="ml-3"><h1>Welcome to iravimar</h1></Container>
                 <Row>
                     {products.map( product =>(
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
