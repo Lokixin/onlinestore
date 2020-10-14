125f510ef528cc02900342a31c00205208e8011b
Dimas Ávila <dimasavilamartinez@gmail.com>
2020-10-14 19:49:33 +0200

    Backed -> Registro, Inicio de Sesión y Autenticación de usuarios añadidos

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,8 +10,8 @@ const errorHandler = (err, req, res, next) => {
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null: err.stack
-    }) 
-    next()
+    })
+    res.end()
 }
 
 export  {notFound, errorHandler}
