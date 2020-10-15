bc5751ac73f016a1306867d426deb5aff99678c5
Dimas Ávila <dimasavilamartinez@gmail.com>
2020-10-15 18:14:31 +0200

    Frontend -> User Registration Screen, user registration constants, actions and reducers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {LinkContainer} from 'react-router-bootstrap'
 import { Navbar, Nav, NavDropdown, Button, Form, FormControl, Container} from 'react-bootstrap'
+import {logout} from '../actions/userActions'
 
 const Header = () => {
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
                 <LinkContainer to="/" ><Navbar.Brand className="ml-3">Iravimart</Navbar.Brand></LinkContainer>
-                
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                     <LinkContainer to="/cart" ><Nav.Link> <i className="fas fa-shopping-cart"></i> Cart</Nav.Link></LinkContainer>
-                    <LinkContainer to="/login" ><Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link></LinkContainer>
+                    {userInfo ? (
+                        <NavDropdown title={userInfo.name} id='username'>
+                            <LinkContainer to='/prodfile'>
+                                <NavDropdown.Item>Profile</NavDropdown.Item>
+                            </LinkContainer>
+                            <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    ): <LinkContainer to="/login" ><Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link></LinkContainer> }
+                    
                     </Nav>
                 </Navbar.Collapse>
             </Container>
