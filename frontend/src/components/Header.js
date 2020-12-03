9ed222dea3ab51e4e9eaeac1c82dd1d90be2b473
Dimas Ávila <dimasavilamartinez@gmail.com>
2020-12-03 18:35:11 +0100

    User update, delete and getUserById

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {LinkContainer} from 'react-router-bootstrap'
-import { Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
-import {logout} from '../actions/userActions'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
     const userLogin = useSelector(state => state.userLogin)
-    const {userInfo} = userLogin
+    const { userInfo } = userLogin
     const dispatch = useDispatch()
     const logoutHandler = () => {
         dispatch(logout())
@@ -14,24 +14,37 @@ const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <LinkContainer to="/" ><Navbar.Brand className="ml-3">Iravimart</Navbar.Brand></LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                    <LinkContainer to="/cart" ><Nav.Link> <i className="fas fa-shopping-cart"></i> Cart</Nav.Link></LinkContainer>
-                    {userInfo ? (
-                        <NavDropdown title={userInfo.name} id='username'>
-                            <LinkContainer to='/profile'>
-                                <NavDropdown.Item>Profile</NavDropdown.Item>
-                            </LinkContainer>
-                            <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-                        </NavDropdown>
-                    ): <LinkContainer to="/login" ><Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link></LinkContainer> }
-                    
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                <Container>
+                    <LinkContainer to="/" ><Navbar.Brand className="ml-3">Iravimart</Navbar.Brand></LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <LinkContainer to="/cart" ><Nav.Link> <i className="fas fa-shopping-cart"></i> Cart</Nav.Link></LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : <LinkContainer to="/login" ><Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link></LinkContainer>}
+                            {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title='Admin' id='adminmenu'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/orders'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}       
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
